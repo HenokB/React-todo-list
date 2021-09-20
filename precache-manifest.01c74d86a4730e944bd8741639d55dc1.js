f4ce21106af5265db5f0ed219cbd7176be5c0bb6
HenokB <henokb2124@gmail.com>
2021-09-20 04:17:35 -0400

    Updates

---
+++ precache-manifest.01c74d86a4730e944bd8741639d55dc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f47127025431218e87c65d40c4162e",
+    "url": "/React-todo-list/index.html"
+  },
+  {
+    "revision": "65065808f46e521616ba",
+    "url": "/React-todo-list/static/css/main.395d53f4.chunk.css"
+  },
+  {
+    "revision": "6f4fb43575dfec3aef03",
+    "url": "/React-todo-list/static/js/2.60f675a1.chunk.js"
+  },
+  {
+    "revision": "65065808f46e521616ba",
+    "url": "/React-todo-list/static/js/main.31fe9427.chunk.js"
+  },
+  {
+    "revision": "a26ea031b757c28c4998",
+    "url": "/React-todo-list/static/js/runtime~main.884aa81e.js"
+  },
+  {
+    "revision": "ed5d468c4e5332f7ae2e4ab0fe91f732",
+    "url": "/React-todo-list/static/media/2.ed5d468c.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-todo-list/static/media/logo.ee7cd8ed.svg"
+  }
+]);
